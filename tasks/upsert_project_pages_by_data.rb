66b0e9b3e9c8859bcb888d80689bd5acab82a602
たくや <takuyadayo014@gmail.com>
2023-08-12 11:51:25 +0900

    exhibitionのレイアウトとymlファイルを変更更新しました。

--- tasks/upsert_project_pages_by_data.rb
+++ tasks/upsert_project_pages_by_data.rb
@@ -18,27 +18,30 @@ projects.each_with_index do |project, index|
     permalink: /expo/#{project["path_URL"]}
     ---
     <h1 style="padding-top: 100px; padding-bottom: 30px; ">#{project["title"]}</h1>
-    {% if "#{project["tag"]}" contains "Scratch（ゲーム、アニメーションなど）" %}
+    <div class="main_content">
+    {% if "#{project["tag"]}" contains "Scratch（ゲーム、アニメーションなどScratchで作成したものすべて）" %}
     {% if "#{project["URL"]}" contains "https://scratch.mit.edu/projects/" %}
     <div class="scratch-wrapper">
     <iframe src="#{project["URL"]}embed" allowtransparency="true" width="542" height="450" frameborder="0" scrolling="no" allowfullscreen></iframe>
     </div>
     {%else%}
-    <img class='top-img lazyload' src='../img/2023/exhibition/#{project["img"]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 400px;' />    
+    <img class='top-img lazyload' src='../img/2023/exhibition/#{project["img"]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />    
     {% endif %}
     {% else %}
-    <img class='top-img lazyload' src='../img/2023/exhibition/#{project["img"]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 400px;' />
+    <img class='top-img lazyload' src='../img/2023/exhibition/#{project["img"]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
     {% endif %}
     <p>クリエイター:#{project["creator"]}(#{project["affiliation"]})</p>
-    <div class="box">
-      <h5>作品の説明</h5>
-      <p>#{project["text"]}</p>
+    <div class="contents">
+      <div class="box">
+        <h5>作品の説明</h5>
+        <p>#{project["text"]}</p>
+      </div>
+      <div class="box">
+        <h5>参考・参照元、工夫したところ</h5>
+        <p>#{project["description"]}</p>
+      </div>
     </div>
-    <div class="box">
-      <h5>参考・参照元、工夫したところ</h5>
-      <p>#{project["description"]}</p>
     </div>
-
     <a href="#{project["URL"]}" target="_blank" rel="noopener" class="button air">作品URL</a>
     
     <style type="text/css">
@@ -47,10 +50,25 @@ projects.each_with_index do |project, index|
       .next { display: table-cell; color: white; text-align: right;  }
       .nav a:link, .nav a:visited { color: white; }
       iframe{margin: 0 auto}
-      .box{width:60%; margin: 0 auto;padding-bottom:20px;}
+      .box{width:auto; margin: 0 auto;padding-bottom:20px;}
       .box h5{text-align: left;}
+      .box p{text-align: left;}
       .air{margin-bottom:60px;}
-
+      .contents{width:auto; margin: 0 auto;padding:5px;}
+      /*content*/
+      .main_content{
+        display: block;
+        margin: 0 auto 12px;
+        text-align: center;
+        text-decoration: none;
+        width: 40%;
+      }
+      @media screen and (max-width: 1000px) {
+        .main_content {
+         padding: 8px 0;
+         width: 80%;
+       }
+      }      
       /* scratch wrapper */
       @media screen and (max-width: 570px){
         div.scratch-wrapper > iframe {
