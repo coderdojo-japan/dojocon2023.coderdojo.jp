1e8336947be38477a8ba16a31114772ec2a5d5e5
Yohei Yasukawa <yohei@yasslab.jp>
2023-08-18 14:56:48 +0900

    作品展示のOGPにもサムネイル画像を適用した

--- tasks/upsert_project_pages_by_data.rb
+++ tasks/upsert_project_pages_by_data.rb
@@ -15,6 +15,7 @@ projects.each_with_index do |project, index|
     ---
     layout: default
     title: "#{project[:title]}"
+    thumbnail: /img/2023/exhibition/#{project[:img]}
     permalink: /expo/#{project[:path_URL]}
     ---
     <h1 style="padding-top: 100px; padding-bottom: 30px; ">#{project[:title]}</h1>
