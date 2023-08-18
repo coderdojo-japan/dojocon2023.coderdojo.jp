d67f73940115cbe161b6ed53e0a690d1ac1c8721
Yohei Yasukawa <yohei@yasslab.jp>
2023-08-18 14:34:36 +0900

    Cosmetic change: Use symbols to get values from keys in Ruby

--- tasks/upsert_project_pages_by_data.rb
+++ tasks/upsert_project_pages_by_data.rb
@@ -7,52 +7,52 @@ require 'yaml'
 # NOTE: もしファイルパスを変更して過去ファイルを消したい場面があれば使う
 #Dir.glob("_posts/exhibition/*.md").each { |filename| File.delete(filename) }
 
-projects = YAML.load_file("_data/exhibition.yml")
+projects = YAML.load_file("_data/exhibition.yml", symbolize_names: true)
 projects.each_with_index do |project, index|
   # Generate individual project page by data
-  path = "./_pages/exhibition/2023-#{project["path_URL"]}.md"
+  path = "./_pages/exhibition/2023-#{project[:path_URL]}.md"
   page = <<~PROJECT_PAGE
     ---
     layout: default
-    title: "#{project["title"]}"
-    permalink: /expo/#{project["path_URL"]}
+    title: "#{project[:title]}"
+    permalink: /expo/#{project[:path_URL]}
     ---
-    <h1 style="padding-top: 100px; padding-bottom: 30px; ">#{project["title"]}</h1>
+    <h1 style="padding-top: 100px; padding-bottom: 30px; ">#{project[:title]}</h1>
     <div class="main_content">
 
     {% comment %}
     /*スクラッチの埋め込みをしたい場合に用いる。*/
-    {% if "#{project["tag"]}" contains "Scratch" %}
-    {% if "#{project["URL"]}" contains "https://scratch.mit.edu/projects/" %}
+    {% if "#{project[:tag]}" contains "Scratch" %}
+    {% if "#{project[:URL]}" contains "https://scratch.mit.edu/projects/" %}
     <div class="scratch-wrapper">
-    <iframe src="#{project["URL"]}embed" allowtransparency="true" width="542" height="450" frameborder="0" scrolling="no" allowfullscreen></iframe>
+    <iframe src="#{project[:URL]}embed" allowtransparency="true" width="542" height="450" frameborder="0" scrolling="no" allowfullscreen></iframe>
     </div>
     {%else%}
-    <img class='top-img lazyload' src='../img/2023/exhibition/#{project["img"]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
+    <img class='top-img lazyload' src='../img/2023/exhibition/#{project[:img]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
     {% endif %}
     {% else %}
-    <img class='top-img lazyload' src='../img/2023/exhibition/#{project["img"]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
+    <img class='top-img lazyload' src='../img/2023/exhibition/#{project[:img]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
     {% endif %}
     {% endcomment %}
     
-    <a href="#{project["URL"]}" target="_blank" rel="noopener" >
+    <a href="#{project[:URL]}" target="_blank" rel="noopener" >
       <img class='top-img lazyload' loading='lazy' alt='サムネイル画像'
            style='margin-bottom: 10px; border-radius: 6px;width: 100%;'
-           src='../img/2023/exhibition/#{project["img"]}' />
+           src='../img/2023/exhibition/#{project[:img]}' />
     </a>
-    <p>クリエイター: #{project["creator"]}(CoderDojo #{project["affiliation"]})</p>
+    <p>クリエイター: #{project[:creator]}(CoderDojo #{project[:affiliation]})</p>
     <div class="contents">
       <div class="box">
         <h5>作品の説明</h5>
-        <p>#{project["text"]}</p>
+        <p>#{project[:text]}</p>
       </div>
       <div class="box">
         <h5>参考・参照元、工夫したところ</h5>
-        <p>#{project["description"]}</p>
+        <p>#{project[:description]}</p>
       </div>
     </div>
     </div>
-    <a href="#{project["URL"]}" target="_blank" rel="noopener" class="button air">
+    <a href="#{project[:URL]}" target="_blank" rel="noopener" class="button air">
       <i class="fas fa-scroll"></i>
       作品を見る
     </a>
