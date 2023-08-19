fb9432cd3339c0f0ffa24e6e46314901f5e13f2a
yasulab <yasulab@users.noreply.github.com>
2023-08-19 00:31:55 +0000

    deploy: e30add24ca4564e92852f4530077cfd64d941105

--- tasks/upsert_project_pages_by_data.rb
+++ tasks/upsert_project_pages_by_data.rb
@@ -21,56 +21,42 @@ projects.each_with_index do |project, index|
     ---
     <h1 style="padding-top: 100px; padding-bottom: 30px; ">#{project[:title]}</h1>
     <div class="main_content">
+      <a href="#{project[:URL]}" target="_blank" rel="noopener" >
+        <img class='top-img lazyload' loading='lazy' alt='サムネイル画像'
+             style='margin-bottom: 10px; border-radius: 6px;width: 100%;'
+             src='/img/2023/exhibition/#{project[:img]}' />
+      </a>
 
-    {% comment %}
-    /*スクラッチの埋め込みをしたい場合に用いる。*/
-    {% if "#{project[:tag]}" contains "Scratch" %}
-    {% if "#{project[:URL]}" contains "https://scratch.mit.edu/projects/" %}
-    <div class="scratch-wrapper">
-    <iframe src="#{project[:URL]}embed" allowtransparency="true" width="542" height="450" frameborder="0" scrolling="no" allowfullscreen></iframe>
-    </div>
-    {%else%}
-    <img class='top-img lazyload' src='/img/2023/exhibition/#{project[:img]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
-    {% endif %}
-    {% else %}
-    <img class='top-img lazyload' src='/img/2023/exhibition/#{project[:img]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
-    {% endif %}
-    {% endcomment %}
-    
-    <a href="#{project[:URL]}" target="_blank" rel="noopener" >
-      <img class='top-img lazyload' loading='lazy' alt='サムネイル画像'
-           style='margin-bottom: 10px; border-radius: 6px;width: 100%;'
-           src='/img/2023/exhibition/#{project[:img]}' />
-    </a>
-    <p>クリエイター: #{project[:creator]}(CoderDojo #{project[:affiliation]})</p>
-    <div class="contents">
-      <div class="box">
-        <h5>作品の説明</h5>
-        <p>#{project[:text]}</p>
-      </div>
-      <div class="box">
-        <h5>参考・参照元、工夫したところ</h5>
-        <p>#{project[:description]}</p>
+      <p>クリエイター: #{project[:creator]}(CoderDojo #{project[:affiliation]})</p>
+      <div class="contents">
+        <div class="box">
+          <h5>作品の説明</h5>
+          <p>#{project[:text]}</p>
+        </div>
+
+        <div class="box">
+          <h5>参考・参照元、工夫したところ</h5>
+          <p>#{project[:description]}</p>
+        </div>
       </div>
     </div>
+
+    <div style='margin-bottom: 100px;'>
+      <a href="#{project[:URL]}" target="_blank" rel="noopener" class="button air">
+        <i class="fas fa-scroll"></i>
+        作品を見る
+      </a>
+      <a href="https://twitter.com/intent/tweet?hashtags=DojoConJapan,CoderDojo&url=/expo/#{project[:path_URL]}&lang=jp&related=DojoConJapan" target="_blank" rel="noopener" class="button air">
+        <i class="fab fa-twitter"></i>
+        作品を共有する
+      </a>
     </div>
-    <a href="#{project[:URL]}" target="_blank" rel="noopener" class="button air">
-      <i class="fas fa-scroll"></i>
-      作品を見る
-    </a>
-    
+
     <style type="text/css">
-      /* 使っていないスタイルのため一時的にコメントアウト
-      .prev { display: table-cell; color: white; text-align: left;   }
-      .toc  { display: table-cell; color: white; text-align: center; }
-      .next { display: table-cell; color: white; text-align: right;  }
-      .nav a:link, .nav a:visited { color: white; }
-      */
-      iframe{margin: 0 auto}
       .box{width:auto; margin: 0 auto;padding-bottom:20px;}
       .box h5{text-align: left;}
       .box p{text-align: left;}
-      a.air{margin-bottom:100px;}
+      a.air{margin-bottom: 50px;}
       a.air{text-decoration: none;}
       .contents{width:auto; margin: 0 auto;padding:5px;}
       /*content*/
@@ -87,8 +73,10 @@ projects.each_with_index do |project, index|
          width: 80%;
        }
       }
-      /* scratch wrapper */
-      /*今回スクラッチを埋め込まないためコメントアウト
+
+      /* Scratch wrapper */
+      /* スクラッチ埋め込みは未実装のためコメントアウト
+      iframe{margin: 0 auto}
       @media screen and (max-width: 570px){
         div.scratch-wrapper > iframe {
           position: absolute;
@@ -106,7 +94,29 @@ projects.each_with_index do |project, index|
         }
       }
       */
+      /* 隣の作品へのナビゲーションバー */
+      /* ナビゲーションは未実装のためコメントアウト
+      .prev { display: table-cell; color: white; text-align: left;   }
+      .toc  { display: table-cell; color: white; text-align: center; }
+      .next { display: table-cell; color: white; text-align: right;  }
+      .nav a:link, .nav a:visited { color: white; }
+      */
     </style>
+
+    {% comment %}
+    /* スクラッチの埋め込みをしたい場合に用いる。*/
+    {% if "#{project[:tag]}" contains "Scratch" %}
+    {% if "#{project[:URL]}" contains "https://scratch.mit.edu/projects/" %}
+    <div class="scratch-wrapper">
+    <iframe src="#{project[:URL]}embed" allowtransparency="true" width="542" height="450" frameborder="0" scrolling="no" allowfullscreen></iframe>
+    </div>
+    {%else%}
+    <img class='top-img lazyload' src='/img/2023/exhibition/#{project[:img]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
+    {% endif %}
+    {% else %}
+    <img class='top-img lazyload' src='/img/2023/exhibition/#{project[:img]}' alt='サムネイル画像' loading='lazy'  style='margin-bottom: 10px; border-radius: 6px;width: 100%;' />
+    {% endif %}
+    {% endcomment %}
   PROJECT_PAGE
 
   IO.write(path, page)
